3ad9b3d3b9a09a9651d0afb72716a8bd10a4ba5a
deoomen <deoomen@protonmail.com>
2024-11-08 18:18:27 +0100

    feat(mission04): add mission script

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from missions.mission00.mission00 import Mission00
 from missions.mission01.mission01 import Mission01
 from missions.mission02.mission02 import Mission02
 from missions.mission03.mission03 import Mission03
+from missions.mission04.mission04 import Mission04
 
 
 def init_loggers() -> None:
@@ -29,6 +30,7 @@ Which mission do you want to run?
 01
 02
 03 - JSON
+04
 """)
         else:
             mission_number = sys.argv[1]
@@ -41,6 +43,8 @@ Which mission do you want to run?
             mission = Mission02()
         elif '03' == mission_number or '3' == mission_number:
             mission = Mission03()
+        elif '04' == mission_number or '4' == mission_number:
+            mission = Mission04()
         else:
             raise RuntimeError('Unknown mission "{}"'.format(mission_number))
 
---
+++ missions/mission04/mission04.py
@@ -0,0 +1,49 @@
+import logging
+from os import getenv
+from openai import OpenAI
+from pprint import pprint
+
+class Mission04:
+
+    name = '04'
+
+    def run(self) -> None:
+        logging.info('Mission 04')
+
+        llm = OpenAI(api_key=getenv('OPENAI_API_KEY'))
+        answer = llm.chat.completions.create(
+            model='gpt-4o-mini',
+            messages=[
+                {
+                    'role': 'system',
+                    'content':
+                        'You play with user in a game where you are controlling a robot. You task is to move robot around the given by user map from start to the finish.'
+                        'To move the robot write exactly "UP" or "RIGHT" or "DOWN" or "LEFT".'
+                        'The map of labyrinth will be presented as 2d matrix with some characters as objects.'
+                        'Legend:'
+                        '- "o" is start'
+                        '- "F" is finish'
+                        '- "p" is a free space that you can walk in'
+                        '- "X" is an obstacle and you cannot get there'
+                        'You start from "o" and must reach finish at "F".'
+                        'Think how can you reach the finish from start using sequence of moves.'
+                        'Your response should look like this:'
+                        '- your thoughts'
+                        '- more thoughts'
+                        '<RESULT>'
+                        '{'
+                        '   "steps": "UP, RIGHT, DOWN, LEFT'
+                        '}'
+                        '</RESULT>'
+                },
+                { 'role': 'user', 'content': """
+                    [
+                      ['p', 'X', 'p', 'p', 'p', 'p'],
+                      ['p', 'p', 'p', 'X', 'p', 'p'],
+                      ['p', 'X', 'p', 'X', 'p', 'p'],
+                      ['o', 'X', 'p', 'p', 'p', 'F']
+                    ]
+                """ }
+            ],
+        ).choices[0].message.content
+        logging.info(f'Answer is: {answer}')
